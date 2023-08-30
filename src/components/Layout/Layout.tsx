0d59bc1afbddb6a1cb589c27d32fa0bf4eb120f8
Valeriy Lopatko <vlopatko1999@gmail.com>
2023-08-30 17:28:59 +0300

    fix linter issue

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Button } from 'shared/ui/Button';
 
 export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <div>
       <div>put header here</div>
       {children}
+      <Button round={true} size="medium" square={true}>
+        Press me!
+      </Button>
       <div>put footer here</div>
     </div>
   );
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -2,37 +2,34 @@ import { FC, ButtonHTMLAttributes, PropsWithChildren } from 'react';
 import cn from 'classnames';
 import './Button.scss';
 
-interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
-  className?: string,
-  square?: boolean,
-  round?: boolean,
-  size?: 'small' | 'medium' | 'large',
-  active?: boolean,
-  isClear?: boolean
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  square?: boolean;
+  round?: boolean;
+  size?: 'small' | 'medium' | 'large';
+  active?: boolean;
+  isClear?: boolean;
 }
 
 export const Button: FC<PropsWithChildren<IButtonProps>> = (props) => {
-  const {
-    className,
-    isClear,
-    square,
-    round,
-    size,
-    children,
-    ...otherProps
-  } = props;
+  const { className, isClear, square, round, size, children, ...otherProps } =
+    props;
 
   return (
     <button
-      className={cn('button', {
-        ['size-s']: size === 'small',
-        ['size-m']: size === 'medium',
-        ['size-l']: size === 'large',
-        ['button-square']: square,
-        ['clear']: isClear,
+      className={cn(
+        'button',
+        {
+          ['size-s']: size === 'small',
+          ['size-m']: size === 'medium',
+          ['size-l']: size === 'large',
+          ['button-square']: square,
+          ['clear']: isClear,
 
-        ['round']: round
-      }, [className])}
+          ['round']: round,
+        },
+        [className],
+      )}
       {...otherProps}
     >
       {children}
